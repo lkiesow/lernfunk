eaf10e8bdff242fd15a18a3fc3258d31309d9c2c
lkiesow@uos.de <lkiesow@uos.de@b53bb859-ed3e-f3c0-b302-adb0b52f088a>
2011-01-27 02:55:04 +0000

    jquery-ui update
    git-svn-id: https://lernfunk.googlecode.com/svn/trunk@178 b53bb859-ed3e-f3c0-b302-adb0b52f088a

--- portal-ui/user-interface/func/config.php
+++ portal-ui/user-interface/func/config.php
@@ -40,16 +40,18 @@ $includes = '
 	<!-- jQuery UI -->
 	<link type="text/css" href="template/'.$cfg['tplName'].'/css/jquery/jquery-ui-1.7.2.custom.css" rel="Stylesheet" />
 	<script type="text/javascript" src="./func/imports/jquery/jquery-1.4.4.min.js"></script>
-	<script type="text/javascript" src="./func/imports/jquery/jquery-ui-1.7.2.custom.min.js"></script>
+	<script type="text/javascript" src="./func/imports/jquery/jquery-ui-1.7.3.custom.min.js"></script>
 
 	<script type="text/javascript" src="./func/imports/jquery-json/jquery.json-2.2.min.js"></script>
 	<script type="text/javascript" src="./func/imports/jquery-hashchange/jquery.ba-hashchange.min.js"></script>
 	<script type="text/javascript" src="./func/imports/jquery-bbq/jquery.ba-bbq.min.js"></script>
 
 	<!-- fancybox -->
+	<!--
 	<script type="text/javascript" src="./func/imports/jquery-fancybox/jquery.mousewheel-3.0.2.pack.js"></script>
-	<script type="text/javascript" src="./func/imports/jquery-fancybox/jquery.fancybox-1.3.1.js"></script>
+	<script type="text/javascript" src="./func/imports/jquery-fancybox/jquery.fancybox-1.3.1.pack.js"></script>
 	<link rel="stylesheet" type="text/css" href="./func/imports/jquery-fancybox/jquery.fancybox-1.3.1.css" media="screen" />
+	-->
 
 	<link rel="stylesheet" type="text/css" href="template/'.$cfg['tplName'].'/css/style.css" />
 	<script type="text/javascript" src="./func/template-js.php?x=y.js.js"></script>
