ba184c9a85898b5fdfa5afda113d80f28ad06b80
lkiesow@uos.de <lkiesow@uos.de@b53bb859-ed3e-f3c0-b302-adb0b52f088a>
2011-02-16 22:07:39 +0000

    Vorbereitung für AJAX-Upload
    git-svn-id: https://lernfunk.googlecode.com/svn/trunk@223 b53bb859-ed3e-f3c0-b302-adb0b52f088a

--- admin-ui/admin-interface/dev/php/config.php
+++ admin-ui/admin-interface/dev/php/config.php
@@ -1,35 +1,36 @@
 <?php
-/* 
-	Copyright (c) 2006 - 2010  Universitaet Osnabrueck, virtUOS 
-	Authors: Nils Birnbaum, Lars Kiesow, Benjamin Wulff
+	/* 
+		Copyright (c) 2006 - 2010  Universitaet Osnabrueck, virtUOS 
+		Authors: Nils Birnbaum, Lars Kiesow, Benjamin Wulff
 
-	This file is part of Lernfunk. 
+		This file is part of Lernfunk. 
 
-	Lernfunk is free software: you can redistribute it and/or modify 
-	it under the terms of the GNU General Public License as published by 
-	the Free Software Foundation, either version 3 of the License, or 
-	(at your option) any later version. 
+		Lernfunk is free software: you can redistribute it and/or modify 
+		it under the terms of the GNU General Public License as published by 
+		the Free Software Foundation, either version 3 of the License, or 
+		(at your option) any later version. 
 
-	Lernfunk is distributed in the hope that it will be useful, 
-	but WITHOUT ANY WARRANTY; without even the implied warranty of 
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-	GNU General Public License for more details. 
+		Lernfunk is distributed in the hope that it will be useful, 
+		but WITHOUT ANY WARRANTY; without even the implied warranty of 
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+		GNU General Public License for more details. 
 
-	You should have received a copy of the GNU General Public License 
-	along with Lernfunk.  If not, see <http://www.gnu.org/licenses/>. 
-*/
+		You should have received a copy of the GNU General Public License 
+		along with Lernfunk.  If not, see <http://www.gnu.org/licenses/>. 
+	*/
 
-// set error mode
-// you may consider to disable error messages in the release build
-// but then useres cannot report any bugs properly
-error_reporting(E_ALL);
-ini_set("display_errors", TRUE);
+	// set error mode
+	// you may consider to disable error messages in the release build
+	// but then useres cannot report any bugs properly
+	error_reporting(E_ALL);
+	ini_set("display_errors", TRUE);
 
-// set the encoding of the output
-//mb_internal_encoding('UTF-8');
-//mb_http_output('UTF8');
+	// set the encoding of the output
+	//mb_internal_encoding('UTF-8');
+	//mb_http_output('UTF8');
 
-// include lernfunk library
-include_once('../lflib/lernfunk.php');
+	// include lernfunk library
+	include_once('../lflib/lernfunk.php');
 
+	$uploaddir = '/var/www/html/release/static/preview';
 ?>
---
+++ admin-ui/admin-interface/dev/php/upload.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(dirname(__FILE__).'/config.php');
+
+
+/*
+if ( (!array_key_exists('user', $_REQUEST)) ||
+	(!array_key_exists('passwd', $_REQUEST)) ||
+	($_REQUEST['user'] != __ADMIN_USER__) ||
+	($_REQUEST['passwd'] != __ADMIN_PASSWD__) )
+	die('error');
+ */
+
+if (!array_key_exists('id', $_REQUEST))
+	die('error');
+
+$id = $_REQUEST['id'];
+$uploadfile = $uploaddir . $id . '/'. basename($_FILES['file0']['name']);
+
+if (!is_dir( $uploaddir.$id ))
+	mkdir( $uploaddir.$id, 0777 );
+chmod( $uploaddir.$id, 0777 );
+
+if (move_uploaded_file($_FILES['file0']['tmp_name'], $uploadfile)) {
+  echo "success";
+} else {
+  // WARNING! DO NOT USE "FALSE" STRING AS A RESPONSE!
+  // Otherwise onSubmit event will not be fired
+	//echo "\n".$_FILES['file0']['tmp_name']."\n";
+	//echo "\n".$uploadfile."\n";
+  echo "error";
+}
