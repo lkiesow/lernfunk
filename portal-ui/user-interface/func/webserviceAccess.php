d48d814ef03d5638fa22aa9d87b66cde5f61a737
lkiesow@uos.de <lkiesow@uos.de@b53bb859-ed3e-f3c0-b302-adb0b52f088a>
2011-03-09 00:17:44 +0000

    gzip compression in proxy
    git-svn-id: https://lernfunk.googlecode.com/svn/trunk@244 b53bb859-ed3e-f3c0-b302-adb0b52f088a

--- portal-ui/user-interface/func/webserviceAccess.php
+++ portal-ui/user-interface/func/webserviceAccess.php
@@ -1,30 +1,57 @@
 <?php
-   if (!array_key_exists('request' , $_REQUEST) || !array_key_exists('url' , $_REQUEST))
-      exit();
+if (!array_key_exists('request' , $_REQUEST) || !array_key_exists('url' , $_REQUEST))
+	exit();
 
-   header('Content-Type: text/plain; charset=utf-8');
-   $src = get_src(stripslashes($_REQUEST['url']).'?request='.urlencode(stripslashes($_REQUEST['request'])));
-   $pos = strpos($src, '{') ;
-   if ($pos !== false) {
-      echo substr($src, $pos);
-   }
+$src = get_url(stripslashes($_REQUEST['url'])
+	.'?request='.urlencode(stripslashes($_REQUEST['request'])));
 
-function get_src($url) {
-	preg_match('@^(?:http://)(?<host>[^/]+)(?<path>/.*)@i', $url, $url);
-
-	$fp = fsockopen ($url['host'], 80, $errno, $errstr, 30);
-	if (!$fp) {
-        $buffer = array($errstr.' ('.$errno.")\n");
-    } else {
-        $i = 0;
-        $buffer = array();
-        fputs ($fp, 'GET '.$url['path']." HTTP/1.0\r\nHost: ".$url['host']."\r\n\r\n");
-        while (!feof($fp)) {
-            $buffer[$i] = fgets($fp,1024);
-            $i++;
-        }
-        fclose($fp);
-    }
-    return implode($buffer, '');
+$gzip = 0;
+foreach( $src['header'] as $h ) {
+	if ( substr( strtolower($h), 0, 13 ) == 'content-type:' ) {
+		header( $h );
+	}
+	if ( substr( strtolower($h), 0, 22 ) == 'content-encoding: gzip' ) {
+		$gzip = 1;
+		header( $h );
+	}
 }
+if ( $gzip ) {
+	echo gzencode($src['content']);
+} else {
+	echo $src['content'];
+}
+
+/**
+ * Get url content and response headers (given a url, follows all redirections 
+ * on it and returned content and response headers of final url)
+ **/
+function get_url( $url,  $javascript_loop = 0, $timeout = 5 )
+{
+	$url = str_replace( "&amp;", "&", urldecode(trim($url)) );
+
+	$cookie = tempnam ("/tmp", "CURLCOOKIE");
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_USERAGENT,      'Opera/9.80 (X11; Linux x86_64; U; en) Presto/2.7.62 Version/11.01' );
+	curl_setopt( $ch, CURLOPT_URL,            $url );
+	curl_setopt( $ch, CURLOPT_COOKIEJAR,      $cookie );
+	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+	curl_setopt( $ch, CURLOPT_ENCODING,       '' );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_AUTOREFERER,    true );
+	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false ); // required for https urls
+	curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+	curl_setopt( $ch, CURLOPT_TIMEOUT,        $timeout );
+	curl_setopt( $ch, CURLOPT_MAXREDIRS,      10 );
+
+	$content = curl_exec( $ch );
+	$response = curl_getinfo( $ch );
+	curl_close ( $ch );
+
+	return array( 
+		'content' => $content, 
+		'info'    => $response,
+		'header'  => get_headers($response['url'])
+	);
+}
+
 ?>
