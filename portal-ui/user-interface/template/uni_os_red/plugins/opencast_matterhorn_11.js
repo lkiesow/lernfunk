97968a53167d97d0f46271be39499c4bf9925e82
lkiesow@uos.de <lkiesow@uos.de@b53bb859-ed3e-f3c0-b302-adb0b52f088a>
2011-04-18 14:50:32 +0000

    opencast plugin fix…
    git-svn-id: https://lernfunk.googlecode.com/svn/trunk@307 b53bb859-ed3e-f3c0-b302-adb0b52f088a

--- portal-ui/user-interface/template/uni_os_red/plugins/opencast_matterhorn_11.js
+++ portal-ui/user-interface/template/uni_os_red/plugins/opencast_matterhorn_11.js
@@ -1,12 +1,15 @@
 function plugin_opencast_matterhorn_11( site, url, preview_url ) {
+
 	if ( site.toLowerCase() == 'seriesdetails' ) {
 		return '<iframe src="' + preview_url + '" '
 			+ 'style="border: none; width: 100%; height: 404px;" '
 			+ 'name="Opencast Matterhorn - Media Player"/>';
+
 	} else if ( site.toLowerCase() == 'home' ) {
-		return '<iframe src="' + preview_url + '" '
-			+ 'style="width: 300px; height: 280px; border: none;"/>';
+		return '<iframe src="' + preview_url + '&amp;play=true" '
+			+ 'style="width: 300px; height: 260px; border: none;"/>';
 	}
+
 }
 
-bindPlayerPlugin( 'Opencast Matterhorn 1.1', 'plugin_opencast_matterhorn_11' );
+bindPlayerPlugin( 'Opencast Matterhorn 1.1', plugin_opencast_matterhorn_11 );
