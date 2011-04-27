ac4e751b3a8ab046f9cc2063c24c8e286ec20db2
lkiesow@uos.de <lkiesow@uos.de@b53bb859-ed3e-f3c0-b302-adb0b52f088a>
2011-04-27 23:28:36 +0000

    + event notifier
    git-svn-id: https://lernfunk.googlecode.com/svn/trunk@319 b53bb859-ed3e-f3c0-b302-adb0b52f088a

---
+++ plug-ins/event-notify/config.php
@@ -0,0 +1,52 @@
+<?php
+/* 
+	Copyright (c) 2006 - 2010  Universitaet Osnabrueck, virtUOS 
+	Authors: Nils Birnbaum, Lars Kiesow, Benjamin Wulff
+
+	This file is part of Lernfunk. 
+
+	Lernfunk is free software: you can redistribute it and/or modify 
+	it under the terms of the GNU General Public License as published by 
+	the Free Software Foundation, either version 3 of the License, or 
+	(at your option) any later version. 
+
+	Lernfunk is distributed in the hope that it will be useful, 
+	but WITHOUT ANY WARRANTY; without even the implied warranty of 
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+	GNU General Public License for more details. 
+
+	You should have received a copy of the GNU General Public License 
+	along with Lernfunk.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+
+// Define general access key
+define('ACCESS_KEY', 'kollateralschaden');
+
+define( 'PORTAL_URL', 'http://www.lernfunk.de/' );
+
+date_default_timezone_set( 'Europe/Berlin' );
+
+// set database access
+$mysql = array(
+      'server' => '',
+      'user'   => '',
+      'passwd' => '',
+      'db'     => 'lernfunk'
+   );
+
+// Set debug state
+if (!defined('__DEBUG__'))
+	define('__DEBUG__', false);
+
+// set error mode
+// you may consider to disable error messages in the release build
+// but then useres cannot report any bugs properly
+error_reporting( E_ALL );
+ini_set( 'display_errors', TRUE );
+
+// set the encoding of the output
+//mb_internal_encoding('UTF-8');
+//mb_http_output('UTF8');
+
+?>
---
+++ plug-ins/event-notify/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Keys: user, passwd, key, cmd
+ * Cmds: get, set
+ **/
+
+header('Content-Type: text/plain; charset=utf-8');
+include_once( dirname(__FILE__).'/feed.php' );
+
+
+if ( !array_key_exists( 'series', $_REQUEST ) ) {
+	die( 'ERROR     : You must specify a series…' );
+}
+LFFeedGenerator::createFeed( intval($_REQUEST[ 'series' ]) );
+
+
+?>
