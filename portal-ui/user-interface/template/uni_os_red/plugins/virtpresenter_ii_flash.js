730e0026f3b48bea98066c508811879783794167
lkiesow@uos.de <lkiesow@uos.de@b53bb859-ed3e-f3c0-b302-adb0b52f088a>
2011-09-06 11:58:57 +0000

    virtpresenter_ii_flash-plugin: Player changed to JWPlayer
    git-svn-id: https://lernfunk.googlecode.com/svn/trunk@356 b53bb859-ed3e-f3c0-b302-adb0b52f088a

--- portal-ui/user-interface/template/uni_os_red/plugins/virtpresenter_ii_flash.js
+++ portal-ui/user-interface/template/uni_os_red/plugins/virtpresenter_ii_flash.js
@@ -1,13 +1,33 @@
-function plugin_virtpresenter_ii_flash( site, url ) {
+function plugin_virtpresenter_ii_flash( site, url, preview_url ) {
+
+	var flashvars = '';
+	var rtmp = preview_url.match( /^rtmp:\/\/[^&]+&url=.*$/ );
+	if (rtmp) {
+		rtmp = rtmp[0].split( '&' );
+		flashvars = rtmp[1].slice( 4 ) + '&amp;streamer=' + rtmp[0];
+	}
 
 	if ( site.toLowerCase() == 'seriesdetails' ) {
-		return '<iframe src="' + url + '" '
-			+ 'style="border: none; width: 100%; height: 404px;" '
-			+ 'name="Opencast Matterhorn - Media Player"/>';
+		return '<object id="player" type="application/x-shockwave-flash" '
+			+ 'data="app/jwplayer/player.swf" style="width: 100%; height: 360px;"> '
+			+ '<param name="allowScriptAccess" value="always"/>'
+			+ '<param name="allowFullScreen" value="true"/>'
+			+ '<param name="movie" value="app/jwplayer/player.swf"/>'
+			+ '<param name="quality" value="high"/>'
+			+ '<param name="bgcolor" value="#ffffff"/>'
+			+ '<param name="flashvars" value="file=' + flashvars + '&amp;autostart=true"/>'
+			+ '</object>';
 
 	} else if ( site.toLowerCase() == 'home' ) {
-		return '<iframe src="' + url + '" '
-			+ 'style="width: 300px; height: 280px; border: none;"/>';
+		return '<object id="player" type="application/x-shockwave-flash" '
+			+ 'data="app/jwplayer/player.swf" style="width: 280px; height: 210px;"> '
+			+ '<param name="allowScriptAccess" value="always"/>'
+			+ '<param name="allowFullScreen" value="true"/>'
+			+ '<param name="movie" value="app/jwplayer/player.swf"/>'
+			+ '<param name="quality" value="high"/>'
+			+ '<param name="bgcolor" value="#ffffff"/>'
+			+ '<param name="flashvars" value="file=' + flashvars + '&amp;autostart=true"/>'
+			+ '</object>';
 	}
 
 }
