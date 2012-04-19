22bb68d993515518f358440c69bc2fa44d2c7742
Lars Kiesow <lkiesow@uos.de>
2012-04-19 15:32:33 +0200

    Fixed scrollbar issue for new  matterhorn player

--- portal-ui/user-interface/template/uni_os_red/plugins/opencast_matterhorn_11.js
+++ portal-ui/user-interface/template/uni_os_red/plugins/opencast_matterhorn_11.js
@@ -1,13 +1,13 @@
 function plugin_opencast_matterhorn_11( site, url, preview_url ) {
 
 	if ( site.toLowerCase() == 'seriesdetails' ) {
-		return '<iframe src="' + preview_url + '" '
+		return '<iframe scrolling="no" src="' + preview_url + '" '
 			+ 'style="border: none; width: 100%; height: 404px;" '
 			+ 'name="Opencast Matterhorn - Media Player" '
 			+ 'class="matterhornplayer11"/>';
 
 	} else if ( site.toLowerCase() == 'home' ) {
-		return '<iframe src="' + preview_url + '&amp;play=true" '
+		return '<iframe scrolling="no" src="' + preview_url + '&amp;play=true" '
 			+ 'style="width: 300px; height: 260px; border: none;"/>';
 	}
 
