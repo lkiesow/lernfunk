90e8108c7c7e842c2127b747aa0e310011067f85
Lars Kiesow <lkiesow@uos.de>
2013-04-17 16:56:15 +0200

    lf-mh-proxy: now with authetication

--- plug-ins/lernfunk-matterhorn-search-proxy/proxy.py
+++ plug-ins/lernfunk-matterhorn-search-proxy/proxy.py
@@ -1,33 +1,95 @@
 import os
+from Crypto.Cipher import AES
+import base64
+from time import time
+import json
+import Cookie
 
+user = { 'student' : 'secret' }
+
+# Time in seconds until the session expires
+login_time = 60 * 60 * 2
 
 __dir__         = os.path.dirname(__file__)
-__seriesdir__   = os.path.join(__dir__,"series/1/")
-__episodesdir__ = os.path.join(__dir__,"episodes/1/")
+__seriesdir__   = os.path.join(__dir__,"series/")
+__episodesdir__ = os.path.join(__dir__,"episodes/")
 
 
-def get_xml(path,req,q=None,offset=0,limit=999999):
+''' Crypto related stuff --------------------------------------------------- '''
+''' Crypto related stuff --------------------------------------------------- '''
+''' Crypto related stuff --------------------------------------------------- '''
+
+# the block size for the cipher object; must be 16, 24, or 32 for AES
+BLOCK_SIZE = 32
+
+# the character used for padding--with a block cipher such as AES, the value
+# you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
+# used to ensure that your value is always a multiple of BLOCK_SIZE
+PADDING = '{'
+
+# one-liner to sufficiently pad the text to be encrypted
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+
+# one-liners to encrypt/encode and decrypt/decode a string
+# encrypt with AES, encode with base64
+EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+
+# generate a random secret key
+#secret = os.urandom(BLOCK_SIZE)
+secret = '\xdc\xe3(\xce\x07\xf2p\xae\xd0\xe8\xf2\xd6E\x91\xb3\xd8\xc0\xd2\xcd\x83,\x8e\xd79:(m\xcan\xae\xa9\xfb'
+
+# create a cipher object using the random secret
+cipher = AES.new(secret)
+
+''' Crypto related stuff --------------------------------------------------- '''
+''' Crypto related stuff --------------------------------------------------- '''
+''' Crypto related stuff --------------------------------------------------- '''
+
+def get_xml(path,req,q=None,id=None,offset=0,limit=999999):
+	logged_in = None
+	cookie = req.headers_in.get('Cookie')
+	if cookie:
+		C = Cookie.SimpleCookie()
+		C.load(cookie)
+		cookie = C.get('JSESSIONID').value
+		try:
+			username, password, expires = json.loads(DecodeAES( cipher, cookie ))
+			logged_in = ( username, password, expires ) if expires > time() else None
+		except:
+			pass
+
 	limit  = int(limit)
 	offset = int(offset)
-	files = os.listdir(path)
+	files = [ (f, '1/' + f) for f in os.listdir(path + '1/') ]
+	if logged_in:
+		files += [ (f, '2/' + f) for f in os.listdir(path + '2/') ]
 	files.sort(reverse=True)
 
 	head  = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
 	head += '<ns2:search-results xmlns:ns2="http://search.opencastproject.org" ' \
-			'xmlns:ns3="http://mediapackage.opencastproject.org" '
+			'xmlns="http://mediapackage.opencastproject.org" '
 	body = ''
 
-	if not q:
+	if id:
+		for n,s in files:
+			if s.endswith('_' + id):
+				f = open( path + s, 'r' )
+				body += f.read()
+				f.close()
+		head += 'searchTime="1" total="%(total)d" limit="%(limit)d" offset="%(offset)d">' % \
+				{ 'total': (1 if body else 0) , 'limit':limit, 'offset':0 }
+	elif not q:
 		head += 'searchTime="1" total="%(total)d" limit="%(limit)d" offset="%(offset)d">' % \
 				{ 'total':len(files), 'limit':limit, 'offset':offset }
 		limit = min( limit, len(files) - offset )
-		for s in files[offset:offset+limit]:
+		for n,s in files[offset:offset+limit]:
 			f = open( path + s, 'r' )
 			body += f.read()
 			f.close()
 	else:
 		count = 0
-		for s in files:
+		for n,s in files:
 			f = open( path + s, 'r' )
 			res = f.read()
 			f.close()
@@ -39,7 +101,11 @@ def get_xml(path,req,q=None,offset=0,limit=999999):
 		head += 'searchTime="1" total="%(total)d" limit="%(limit)d" offset="%(offset)d">' % \
 				{ 'total':count, 'limit':limit, 'offset':offset }
 		
-	head += '<query>*:*</query>';
+	if logged_in:
+		head += '<query>*:* AND (oc_acl_read:ROLE_USER)</query>'
+	else:
+		head += '<query>*:* AND (oc_acl_read:ROLE_ANONYMOUS)</query>'
+
 	foot  = '</ns2:search-results>'
 
 	req.content_type = 'application/xml'
@@ -50,7 +116,7 @@ class series:
 	def xml(self,req,q=None,offset=0,limit=999999,id=None):
 		if id:
 			q = ' id="' + id + '"'
-		return get_xml(__seriesdir__,req,q,offset,limit)
+		return get_xml(__seriesdir__,req,q,id,offset,limit)
 
 
 class episode:
@@ -59,7 +125,7 @@ class episode:
 			q = '<series>' + q + '</series>'
 		if id:
 			q = ' id="' + id + '"'
-		return get_xml(__episodesdir__,req,q,offset,limit)
+		return get_xml(__episodesdir__,req,q,id,offset,limit)
 
 
 class search:
@@ -75,3 +141,22 @@ search  = search()
 def index(req):
 	req.content_type = 'text/plain'
 	return "success"
+
+def j_spring_security_check(req,j_username, j_password):
+	if not j_username in user.keys():
+		req.status = 401
+		return 'Invalid username'
+	if user[j_username] != j_password:
+		req.status = 401
+		return 'Invalid password'
+
+	# success
+
+	# generate data
+	expires = int(time()) + login_time
+	data = json.dumps( [ j_username, j_password, expires ] )
+
+	# encrypt data
+	encdata = EncodeAES( cipher, data )
+	req.headers_out['Set-Cookie'] = 'JSESSIONID=%s;Path=/' % encdata
+	return ''
